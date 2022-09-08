9a62bf9a6eacf6394adc26390ed255a28e6bd7f0
Mateus Silva <gmateusjose6@gmail.com>
2022-09-08 20:36:16 -0300

    3.4 formatting and enveloping responses

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -4,18 +4,18 @@ import (
 	"net/http"
 )
 
-// Declare a handler which writes a JSON response with information about
-// the application status, operating environment and version.
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
 	// Create a map which holds the information that we want to send in the
 	// response
-	data := map[string]string{
-		"status":      "available",
-		"environment": app.config.env,
-		"version":     version,
+	env := envelope{
+		"status": "available",
+		"system_info": map[string]string{
+			"environment": app.config.env,
+			"version":     version,
+		},
 	}
 
-	err := app.writeJSON(w, http.StatusOK, data, nil)
+	err := app.writeJSON(w, http.StatusOK, env, nil)
 	if err != nil {
 		app.logger.Println(err)
 		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -32,32 +32,21 @@ func (app *application) readIDParam(r *http.Request) (int64, error) {
 	return id, nil
 }
 
-// Define a writeJSON() helper for sending responses. This takes the destination
-// http.ResponseWriter, the http status code to send, the data to encode to
-// json, and a header map containing any additional http headers we want to
-// include in the response.
+// Define an envelope type
+type envelope map[string]interface{}
+
+// Change the data parameter to have the type envelope instead of interface{}
 func (app *application) writeJSON(w http.ResponseWriter, status int, data interface{}, headers http.Header) error {
-	// Encode the data to json, returning the error if there was one.
-	js, err := json.Marshal(data)
+	js, err := json.MarshalIndent(data, "", "\t")
 	if err != nil {
 		return err
 	}
 
-	// Append a newline to make it easier to view in terminal applications
 	js = append(js, '\n')
-
-	// At this point, we know that we won't encounter any more errors before
-	// writing the response, so it's safe to add any headers that we want to
-	// include. We loop through the header map and add each header to the
-	// http.ResponseWriter header map. Note that it's OK if the provider header
-	// map is nil. Go doesn't throw an error if you try to range over (or
-	// generally, read from) a nil map.
 	for key, value := range headers {
 		w.Header()[key] = value
 	}
 
-	// Add the "Content-Type: application/json" header, then write the status
-	// code and json response.
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(status)
 	w.Write(js)
