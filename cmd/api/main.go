635f8c59a600fa04ae6ee0a67f1b044f09d0934f
Mateus Silva <gmateusjose6@gmail.com>
2022-09-14 19:42:14 -0300

    7.1 setting up the movie model

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -12,6 +12,7 @@ import (
 
 	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
+	"greenlight.mateussilva/internal/data"
 )
 
 // Declare a string containing the application version number. Later in the
@@ -43,6 +44,7 @@ type config struct {
 type application struct {
 	config config
 	logger *log.Logger
+	models data.Models
 }
 
 func main() {
@@ -83,6 +85,7 @@ func main() {
 	app := &application{
 		config: cfg,
 		logger: logger,
+		models: data.NewModels(db),
 	}
 
 	// Declare a HTTP server with some sensible timeout settings, which listens
